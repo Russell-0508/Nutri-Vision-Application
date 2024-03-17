c25a3b5642baf198736f8e5676c88eea40f936e6
HL_Pan <panhaolun@gmail.com>
2024-03-17 15:41:01 +0800

    Connected firebase to Nutri-Vision

--- Lab 3/Nutri-Vision/firebase/config.js
+++ Lab 3/Nutri-Vision/firebase/config.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyARlb3gidDyk8L6Ip-1-mEHXX2Z-mFA0So",
   authDomain: "nutri-vision-78db7.firebaseapp.com",
@@ -17,5 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseapp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseapp);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
